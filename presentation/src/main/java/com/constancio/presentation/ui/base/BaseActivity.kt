9d9b818ada530ce6e7b6d68224fbf0ce58915bb1
Uryel Constancio <uryelhenrique.c@gmail.com>
2019-11-29 21:20:56 +0000

    Create base activity and view model

---
+++ presentation/src/main/java/com/constancio/presentation/ui/base/BaseActivity.kt
@@ -0,0 +1,33 @@
+package com.constancio.presentation.ui.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import io.reactivex.disposables.CompositeDisposable
+
+/**
+ * Base implementation for all activities in the application.
+ */
+abstract class BaseActivity<B : ViewDataBinding> : AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    protected val compositeDisposable = CompositeDisposable()
+
+    abstract fun getLayoutRes(): Int
+
+    /**
+     * Called when the activity is starting.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, getLayoutRes())
+    }
+
+    /** The final call you receive before your activity is destroyed. */
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
+    }
+}

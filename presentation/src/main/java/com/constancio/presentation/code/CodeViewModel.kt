43009aa0689489f11408d41a2b45f366c5bbead2
Uryel Constancio <uryelhenrique.c@gmail.com>
2019-11-30 13:39:57 +0000

    Implement code view model and activity

--- presentation/src/main/java/com/constancio/presentation/code/CodeViewModel.kt
+++ presentation/src/main/java/com/constancio/presentation/code/CodeViewModel.kt
@@ -1,7 +1,46 @@
 package com.constancio.presentation.code
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.constancio.domain.exception.DefaultException
+import com.constancio.domain.interaction.CodeUseCase
 import com.constancio.presentation.ui.base.BaseViewModel
+import com.constancio.presentation.utils.SingleLiveData
 
-class CodeViewModel : BaseViewModel() {
+class CodeViewModel(
+    private val codeUseCase: CodeUseCase
+) : BaseViewModel() {
 
+    private val _code = MutableLiveData<String>()
+    val responseCode: LiveData<String> get() = _code
+
+    val error = SingleLiveData<DefaultException>()
+
+    fun loadPath() {
+        subscribeCompletable(
+            observable = codeUseCase.getNextPath()
+                .doOnSubscribe { showLoading.set(true) }
+                .doFinally { showLoading.set(false) },
+            complete = {
+                //todo
+            },
+            error = { error.postValue(it) }
+        )
+    }
+
+    fun fetchResponseCode() {
+        subscribeSingle(
+            observable = codeUseCase.getResponseCode()
+                .doOnSubscribe { showLoading.set(true) }
+                .doFinally { showLoading.set(false) },
+            success = {
+                _code.postValue(it.responseCode)
+            },
+            error = { error.postValue(it) }
+        )
+    }
+
+    fun onClickFetchCode() {
+        fetchResponseCode()
+    }
 }
